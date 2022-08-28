ea33d09980f248602c6e58f1af5b76a2d7501dba
helios2k <nlongp2109@gmail.com>
2022-08-27 23:55:13 -0400

    structure components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,10 +3,15 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   )
 }
