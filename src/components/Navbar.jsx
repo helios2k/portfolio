a0c951b3372cc792e5d0e8036bfe3e17c9ac537a
helios2k <nlongp2109@gmail.com>
2022-08-30 02:59:24 -0400

    React router & minor UX improve

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,56 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import logo from '../assets/logo.png'
 import '../styles/navbar.scss'
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false)
   const [mobileMenuActive, setMobileMenuActive] = useState(false)
+  const { pathname } = useLocation()
 
-  const handleMenuAction = () => {
-    setMobileMenuActive(!mobileMenuActive)
-  }
+  // Apply navbar background for text visibility after a scroll distance
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 600) {
+        setScrolled(true)
+      } else setScrolled(false)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  // Re-render / close mobile menu when path changes
+  useEffect(() => {
+    setMobileMenuActive(false)
+  }, [pathname])
   return (
-    <nav className={`navbar ${mobileMenuActive ? 'activeMobile' : ''}`}>
+    <nav className={`navbar ${scrolled ? 'scrolled' : ''} ${mobileMenuActive ? 'activeMobile' : ''}`}> 
       <div className='navbar__logo'>
-        <a href='/'>
+        <Link to='/'>
           <img src={logo} alt='logo' />
-        </a>
+        </Link>
       </div>
 
       <ul className='navbar__links'>
         <li>
-          <a href='/' className='navbar__links__item'>Home</a>
+          <Link to='/' className='navbar__links__item'>Home</Link>
         </li>
         <li>
-          <a href='/about' className='navbar__links__item'>About</a>
+          <Link to='/about' className='navbar__links__item'>About</Link>
         </li>
         <li>
-          <a href='/projects' className='navbar__links__item'>Projects</a>
+          <Link to='/projects' className='navbar__links__item'>Projects</Link>
         </li>
         <li>
-          <a href='/contact'>
+          <Link to='/contact'>
             <button className='navbar__btn'>Contact me!</button>
-          </a>
+          </Link>
         </li>
       </ul>
-      <div className='burger-menu' onClick={handleMenuAction}>
+      <div className='burger-menu' onClick={() => setMobileMenuActive(!mobileMenuActive)}>
         {mobileMenuActive ? <FontAwesomeIcon icon={faTimes} /> : <FontAwesomeIcon icon={faBars} />}
       </div>
     </nav>
