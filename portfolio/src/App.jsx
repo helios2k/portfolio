1acc1b691a831e1dbb0c1eb1510d9febd0e9a824
helios2k <nlongp2109@gmail.com>
2022-08-30 00:13:17 -0400

    set up react router navbar navigation

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import React from "react"
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-    </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
