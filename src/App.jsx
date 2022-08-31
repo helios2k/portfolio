654b0e63ee0af75117782dec3494819a74aa99b1
helios2k <nlongp2109@gmail.com>
2022-08-31 14:09:31 -0400

    scroll to top func every time route changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home"
 import About from "./components/About"
 import Projects from "./components/Projects"
 import Contact from "./components/Contact"
+import ScrollToTop from "./components/ScrollToTop"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import React from "react"
 
@@ -10,6 +11,7 @@ function App() {
 
   return (
       <BrowserRouter>
+      <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
